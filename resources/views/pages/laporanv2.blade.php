1d02f162f75c0e4fc43f4072b2f19a93b80e1ae4
MohamadSyahrul <mochamadsahrul28@gmail.com>
2022-08-29 22:53:49 +0700

    hallo

--- resources/views/pages/laporanv2.blade.php
+++ resources/views/pages/laporanv2.blade.php
@@ -1,11 +1,7 @@
 @extends('layout.master')
 @section('title')
-Pasien KB
+Laporan
 @endsection
-{{-- @push('plugin-style')
-    <link id="pagestyle" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css" rel="stylesheet" />
-    <link id="pagestyle" href="https://cdn.datatables.net/1.11.5/css/dataTables.bootstrap4.min.css" rel="stylesheet" />
-@endpush --}}
 @section('content')
 <div class="row">
     <div class="col-12">
